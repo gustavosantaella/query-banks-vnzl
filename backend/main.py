f18fcdf9c6c54a4598ed3eb77d7a373c91325e59
Gustavo Santaella <gustavo.santaella@sirumatek.com>
2026-09-22 08:54:25 -0400

    port

--- backend/main.py
+++ backend/main.py
@@ -1,12 +1,13 @@
 import uvicorn
 from dotenv import load_dotenv
+import os
 
 if __name__ == '__main__':
     load_dotenv()
     uvicorn.run(
         'src.app_module:http_server',
         host="0.0.0.0",
-        port=8000,
+        port=int(os.getenv("APP_PORT")),
         reload=True
     )
     
